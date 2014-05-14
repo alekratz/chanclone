6fa5830fa1d067c929a52b545d435f41996724f6
Alek Ratzloff <alekratz@gmail.com>
2014-05-14 10:29:48 -0400

    Finally got thumbnails and images working. Added thumbnail() method to create a thumbnail if needed, and retrieves the path to a thumbnail if it exists.

--- chanclone.py
+++ chanclone.py
@@ -7,6 +7,10 @@ from werkzeug.routing import BaseConverter
 from werkzeug.utils import secure_filename
 from post import Post
 from board import Board, getBoards, reloadBoardCache
+try:
+  from PIL import Image, ImageOps
+except ImportError:
+  raise RuntimeError('Image module of PIL needs to be installed')
 
 # Load the application
 app = Flask(__name__)
@@ -26,9 +30,10 @@ app.config.update(dict(
   SECRET_KEY='SECRET_DEVELOPMENT_KEY',
   USERNAME='admin',
   PASSWORD='changeme',
-  UPLOAD_FOLDER='static/img'
+  UPLOAD_FOLDER='static/img',
 ))
 
+# Set up the regex converter to be used for regexes
 app.url_map.converters['regex'] = RegexConverter
 
 #
@@ -43,12 +48,46 @@ def get_file_ext(filename):
   else:
     return ''
 
-def verify_path(location):
+def verify_path(location, mkDir=True):
   if os.path.exists(location) and os.path.isdir(location):
-    return
+    return True
+  elif mkDir:
+    os.makedirs(location, 0755)
+    return True
   else:
-    os.mkdir(location)
+    return False
 
+def thumbnail(imgUrl, size="200x200"):
+  width, height = [int(x) for x in size.split('x')]
+  urlPath, imgName = os.path.split(imgUrl)
+  # expecting this to be something like /img/g/1234.jpg or whatever
+  thumbsPath = urlPath.replace('img', 'thumbs', 1)
+
+  localPath = os.path.join("static", urlPath[1:], imgName) # discard leading slash
+  localThumbsPath = os.path.join("static", thumbsPath[1:]) # discard leading slash
+
+  verify_path(localPath, False)
+  verify_path(localThumbsPath) # make the directory if it doesn't exist
+
+  # server-side location
+  thumbFilepath = os.path.join(localThumbsPath, imgName)
+  if(os.path.exists(thumbFilepath)):
+    # client-side url
+    return os.path.join(thumbsPath, imgName)
+
+  # create the file here
+  else:
+    try:
+      image = Image.open(localPath)
+    except IOError:
+      return None
+
+    img = image.copy()
+    img.thumbnail((width, height), Image.ANTIALIAS)
+
+    img.save(thumbFilepath, image.format, quality=85)
+    return os.path.join(thumbsPath, imgName)
+  
 #
 # Database methods
 #
@@ -81,8 +120,18 @@ def init_db():
 #
 # Routing methods
 #
+
+@app.route("/<regex('img|thumbs'):res>/<board>/<regex('[0-9]+\\.[a-zA-Z]+'):filename>")
+def staticImage(res, board, filename):
+  path = os.path.join("static", res, board)
+  return send_from_directory(path, filename)
+
 @app.route("/<board>/")
 def boardPage(board):
+  """
+  Routes to the index page of a board. Prints a list of threads and the
+  last few replies to that thread.
+  """
   b = getBoards(get_db())[board]
   posts = b.getPosts(get_db())
   app.logger.info("Retrieving " + str(len(posts)) + " posts")
@@ -91,6 +140,10 @@ def boardPage(board):
 
 @app.route("/<board>/<regex('[0-9]+'):number>")
 def resPage(board, number):
+  """
+  Gets the page of a thread for responses to that thread. Prints all of
+  the responses.
+  """
   b = getBoards(get_db())[board]
   post = b.getPosts(get_db())[int(number)]
   if post == None:
@@ -101,6 +154,10 @@ def resPage(board, number):
 
 @app.route("/<board>/<regex('[0-9]+'):number>/res", methods=['POST'])
 def respond(board, number):
+  """
+  The post page for after a response was made. Handles making the post
+  and uploads the image if any.
+  """
   title = request.form['title']
   name = "Anonymous" if request.form["name"] == "" else request.form["name"]
   content = request.form["content"]
@@ -111,10 +168,10 @@ def respond(board, number):
   if upload:
     if allowed_file(upload.filename):
       filename = "{0}.{1}".format(int(time.time()), get_file_ext(upload.filename))
-      upload_path = os.path.join(app.config['UPLOAD_FOLDER'], board)
+      upload_path = os.path.join(app.config['UPLOAD_FOLDER'], "img", board)
       verify_path(upload_path)
       upload.save(os.path.join(upload_path, filename))
-      image_src = os.path.join(upload_path, filename)
+      image_src = os.path.join("img", board, filename)
     else:
       return "<center><h1>Error: file must be an image</h1></center>"
 
@@ -132,6 +189,9 @@ def respond(board, number):
 
 @app.route("/<board>/newthread/", methods=['POST'])
 def newthread(board):
+  """
+  Handles creation of a new thread. Must include a file upload. May change later.
+  """
   if request.method == "POST":
     title = request.form["title"]
     name = "Anonymous" if request.form["name"] == "" else request.form["name"]
@@ -146,12 +206,12 @@ def newthread(board):
         upload_path = os.path.join(app.config['UPLOAD_FOLDER'], board)
         verify_path(upload_path)
         upload.save(os.path.join(upload_path, filename))
-        image_src = os.path.join(upload_path, filename)
+        image_src = os.path.join("/", "img", board, filename)
       else:
         return "<center><h1>Error: file must be an image</h1></center>"
 
-    if image_src == None and content == "":
-      return "<center><h1>You must fill out either the file or content form</h1></center>"
+    if image_src == None:
+      return "<center><h1>You must include an image to start a new thread</h1></center>"
     db = get_db()
     db.execute(
       """
@@ -171,4 +231,8 @@ if __name__ == "__main__":
   handler.setLevel(logging.INFO)
   app.logger.addHandler(handler)
   init_db()
+
+  # add thumbnail function to the app
+  app.jinja_env.filters['thumbnail'] = thumbnail
+
   app.run()
